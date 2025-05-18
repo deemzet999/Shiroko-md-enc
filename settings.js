6b323b4f30016f63163b79b54d30b2e6288275dc
deemzet999 <imno71250@gmail.com>
2025-05-18 08:15:45 -0700

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,174 @@
+const fs = require('fs');
+const chalk = require('chalk');
+const { version } = require("./package.json")
+
+//Bot by DimzSelole
+//mau beli no enc 20k saja
+//#HARGAIN JANGAN HAPUS INI!
+
+// Settings Bot 
+global.owner = '6285815977478'
+global.versi = version
+global.namaOwner = "DimzSelole"
+global.packname = 'DimzSelole'
+global.botname = 'Shiroko MD'
+global.botname2 = 'Shiroko MD'
+
+global.tempatDB = 'database.json' // Jangan ubah
+global.pairing_code = true // Jangan ubah
+
+// Settings Link / Tautan
+global.linkOwner = "https://6285815977478"
+global.linkGrup = "https://chat.whatsapp.com/Ex1AKWerfSt83PODb9XZvJ"
+
+// Delay Jpm & Pushctc || 1000 = 1detik
+global.delayJpm = 3000
+global.delayPushkontak = 6000
+
+// Settings Channel / Saluran
+global.linkSaluran = "https://whatsapp.com/channel/0029Vb39eYm7j6g3OI9Y1Y2C"
+global.idSaluran = "120363389269945426@newsletter"
+global.namaSaluran = "</> You Know Dimz?💫` || Buyy-panel✯"
+
+// Tutorial Ngocok Orkut :
+// * Ke CS Orkut @orderkuota 
+// Teks :Kak bisa tolong buatin akun H2H kak? 
+// Ntar disuruh menuliskan nama dan nomer akun orkut lu
+//* klau udh di ksih pencet link okeconnect.com
+//* login okeconnect.com
+//* klik pojok kanan atas (garis tiga) 
+//* klik Payment H2H
+//* klik integrasi API
+//* jadi deh Api orkut mu
+//* yang qris ke website : https://scanqr.org
+//Kamu upload gambar qris orderkuota mu 
+//Ntar ada semacam nomer/api
+//Note :
+//Qris orkut wajib kecetak
+// Info : https://whatsapp.com/channel/0029VakRR89L7UVPwf53TB0v/2682
+global.pinH2H = "-"
+global.passwordH2H = "-"
+global.merchantIdOrderKuota = "OK1916757"
+global.apiOrderKuota = "508792517380208121916757OKCT388147E784BC65D0E9D0735EB0C39FB5"
+global.qrisOrderKuota = "00020101021126670016COM.NOBUBANK.WWW01189360050300000879140214850085847188690303UMI51440014ID.CO.QRIS.WWW0215ID20243345542820303UMI5204541153033605802ID5920BIYU STORE OK19167576008MAGELANG61055611162070703A016304FB3C"
+
+// Settings Api Digital Ocean
+global.apiDigitalOcean = "-"
+
+// Settings Api Digital Ocean
+global.apiSimpelBot = "simplebotz85"
+
+
+// Settings All Payment
+global.shopepay = "Tidak Tersedia"
+global.dana = "6285815977478"
+global.ovo = "6285815977478"
+global.gopay = "6285815977478"
+
+// Settings Image Url
+global.image = {
+menu: "https://img1.pixhost.to/images/5819/600044366_depayyy.jpg", 
+reply: "https://img12.pixhost.to/images/1084/578460624_image.jpg", 
+logo: "https://img12.pixhost.to/images/1084/578461157_image.jpg", 
+dana: "https://img1.pixhost.to/images/5842/600677934_depayyy.jpg", 
+ovo: "https://img1.pixhost.to/images/5842/600677934_depayyy.jpg", 
+gopay: "https://img1.pixhost.to/images/5842/600677934_depayyy.jpg", 
+qris: "https://img12.pixhost.to/images/1084/578461625_image.jpg"
+}
+
+// Settings Api Panel Pterodactyl
+global.egg = "15" // Egg ID
+global.nestid = "5" // nest ID
+global.loc = "1" // Location ID
+global.domain = "https://privatedimzselolekuy.benzzofficial.web.id"
+global.apikey = "ptla_hsbcsCQtZ37WH8xEdxhSxKzfRudp1tfPRQaeGdoVlxG" //ptla
+global.capikey = "ptlc_VpIMW1W0o0oOwkhaL0VbgpAkvjL0doYkXFW1E18f0GQ" //ptlc
+
+// Settings Api Panel Pterodactyl Server 2
+global.eggV2 = "15" // Egg ID
+global.nestidV2 = "5" // nest ID
+global.locV2 = "1" // Location ID
+global.domainV2 = "https://-"
+global.apikeyV2 = "-" //ptla
+global.capikeyV2 = "-" //ptlc
+
+// Settings Api Subdomain
+global.subdomain = {
+"privatehost.us.kg": {
+"zone": "790918217c4add75b7684458518c5836", 
+"apitoken": "qYv4NvEN6ZcUIv4dEXihjkmQMwbP_-3Qy_zFlAHv"
+}, 
+"botwhatsapp.us.kg": {
+"zone": "fb1ac418c5564373a56c91d962b30dca", 
+"apitoken": "rfQih0XNXiq7AyEuDoLjoFfHX2mhYf_9kddAdKIo"
+}, 
+"skyzopedia.us.kg": {
+"zone": "9e4e70b438a65c1d3e6d0e48b82d79de", 
+"apitoken": "odilM9DpvLVPodbPyZwW7UcDKg1aIWsivJc0Vt_o"
+}, 
+"marketplace.us.kg": {
+"zone": "2f33118c3db00b12c38d07cf1c823ed1", 
+"apitoken": "6WS_Op6yuPOWcO17NiO-sOP8Vq9tjSAFZyAn82db"
+}, 
+"pteroserver.us.kg": {
+"zone": "f693559a94aebc553a68c27a3ffe3b55", 
+"apitoken": "ZPAXx7CL51PtbGweL2pE3BsI3x0hgTgLuy56iXuo"
+}, 
+"serverku.biz.id": {
+"zone": "4e4feaba70b41ed78295d2dcc090dd3a", 
+"apitoken": "oof_QRNdUC4aMQ3xIB8dmkGaZu7rk2J-0P_tN55l"
+}, 
+"privatserver.my.id": {
+"zone": "699bb9eb65046a886399c91daacb1968", 
+"apitoken": "CrQMyDn2fhchlGne2ogAw7PvJLsg4x8vasBv__6D"
+}, 
+"panelwebsite.biz.id": {
+"zone": "2d6aab40136299392d66eed44a7b1122", 
+"apitoken": "cj17Lzg9otqwkYIVzgL0pcVA4GfcXqePHAOhCqa_"
+}, 
+"mypanelstore.web.id": {
+"zone": "c61c442d70392500611499c5af816532", 
+"apitoken": "N_VhWv2ZK6UJxLdCnxMfZx9PtzAdmPGM3HmOjZR4"
+}, 
+"pteroserver.us.kg": {
+"zone": "f693559a94aebc553a68c27a3ffe3b55", 
+"apitoken": "qRxwgS3Kl_ziCXti2p4BHbWTvGUYzAuYmVM28ZEp"
+}, 
+"digitalserver.us.kg": {
+"zone": "df13e6e4faa4de9edaeb8e1f05cf1a36", 
+"apitoken": "sH60tbg10UH8gpNrlYpf3UMse1CNJ01EKJ69YVqb"
+}, 
+"biyu-offc.biz.id": {
+"zone": "486604cbd7c410b1d883ce35da2d95f6", 
+"apitoken": "lHSc0-55bsdaUKbXNuY1WUw7RVCPX51h2XMe3EY3"
+}, 
+"shopserver.us.kg": {
+"zone": "54ca38e266bfdf2dcdb7f51fd79c2db5", 
+"apitoken": "4qOupI-Of-6yNrBaeS1-H0KySuKCd0wS-x0P5XQ4"
+}, 
+"devzzhosting.my.id": {
+"zone": "f994c2f66ea24d81ea5f4a17a57a725f", 
+"apitoken": "AqzTJ13YE2ZxmKXdBP4imLhFMFPjN8VUt9mpbTGq"
+}
+}
+
+// Message Command 
+global.mess = {
+	owner: "* *Akses Ditolak*\nFitur ini hanya untuk owner bot!",
+	admin: "* *Akses Ditolak*\nFitur ini hanya untuk admin grup!",
+	botAdmin: "* *Akses Ditolak*\nFitur ini hanya untuk ketika bot menjadi admin!",
+	group: "* *Akses Ditolak*\nFitur ini hanya untuk dalam grup!",
+	private: "* *Akses Ditolak*\nFitur ini hanya untuk dalam private chat!",
+	prem: "* *Akses Ditolak*\nFitur ini hanya untuk user premium!",
+	wait: 'Loading...',
+	error: 'Error!',
+	done: 'Done'
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
